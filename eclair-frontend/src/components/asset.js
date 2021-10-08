0763d6d0f13fb922a0c63374c6e4fbad6ac561f1
SiddanshChawla <chawlasiddansh@gmail.com>
2021-10-08 09:47:45 +0530

    asset-page-edit

--- eclair-frontend/src/components/asset.js
+++ eclair-frontend/src/components/asset.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Flex, Heading, HStack, VStack, Text } from "@chakra-ui/layout";
+import { Flex, Heading, HStack, VStack, Text, Divider } from "@chakra-ui/layout";
 import { Image } from "@chakra-ui/image";
 import { Route } from "react-router";
 import Header from './header';
 import Footer from './footer';
 import data from "../data";
+import { Table, Tbody, Tr, Td } from "@chakra-ui/table";
 
 function asset(props){
     //console.log(props.match.params.id)
@@ -12,16 +13,41 @@ function asset(props){
 
     return <div>
         <Header />
-        <Flex width='full' padding='16' backgroundColor='#19323C' textColor='white' align='center'>
-            <HStack spacing='10'>
-                <Image src={nftSelected.Url} padding='5' paddingLeft='32'/>
-                <VStack width='50%' textAlign='left' align='flex-start'>
-                    <Heading as='h2' color='#F2545B'>{nftSelected.Title}</Heading>
-                    <Heading as='h3' size='md' color='#A93F55'>{nftSelected.Artist}</Heading>
-                    <br/>
-                    <Text textAlign='justify'>{nftSelected.Desc}</Text>
-                </VStack>
-            </HStack>
+        <Flex width='full' padding='16' backgroundColor='#19323C' textColor='white'>
+            <VStack>
+                <HStack spacing='10'>
+                    <Image src={nftSelected.Url} padding='8' marginInlineStart='100px'/>
+                    <VStack width='50%' textAlign='left' align='flex-start'>
+                        <Heading as='h2' color='#F2545B'>{nftSelected.Title}</Heading>
+                        <Heading as='h3' size='md' color='#A93F55'>{nftSelected.Artist}</Heading>
+                        <br/>
+                        <Text textAlign='justify'>{nftSelected.Desc}</Text>
+                    </VStack>
+                </HStack>
+                <HStack>
+                    <Table size='sm' mx='0'>
+                        <Tbody>
+                            <Tr>
+                                <Td>edition(s)</Td>
+                                <Td textAlign='right'>{nftSelected.Editions}</Td>
+                            </Tr>
+                            <Tr>
+                                <Td>type</Td>
+                                <Td textAlign='right'>{nftSelected.Type}</Td>
+                            </Tr>
+                            <Tr>
+                                <Td>royalties</Td>
+                                <Td textAlign='right'>{nftSelected.Royalties}</Td>
+                            </Tr>
+                            <Tr>
+                                <Td>link</Td>
+                                <Td textAlign='right'>{nftSelected.Link}</Td>
+                            </Tr>
+                        </Tbody>
+                    </Table>
+                </HStack>
+                
+            </VStack>
         </Flex>
         
         <Footer />
--- eclair-frontend/src/data.js
+++ eclair-frontend/src/data.js
@@ -1,25 +1,34 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
     nftExplore : [
-    {
-      key: 1,
-      _id: 1,
-      Url: 'https://picsum.photos/250?random=6',
-      Artist: "nsdonviv",
-      Desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      Title: "nft1",
-      Price: "50 $celo"
-    },
-    {
-      key: 2,
-      _id: 2,
-      Url: 'https://picsum.photos/250?random=7',
-      Artist: "nsdonviv",
-      Desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      Title: "nft2",
-      Price: "80 $celo"
-      
-    },
+      {
+        key: 1,
+        _id: 1,
+        Url: 'https://picsum.photos/250?random=6', //this is the image-display-url
+        Artist: "nsdonviv",
+        Desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        Title: "nft1",
+        Price: "50 $celo",
+        Editions: "1",
+        Type: "img",
+        Royalties: "30%",
+        Link: "service-storage.com/cnsodno"
+  
+      },
+      {
+          key: 2,
+          _id: 2,
+          Url: 'https://picsum.photos/250?random=7',
+          Artist: "nsdonviv",
+          Desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          Title: "nft2",
+          Price: "80 $celo",
+          Editions: "10",
+          Type: "img",
+          Royalties: "10%",
+          Link: "service-storage.com/cnsosdcsdvsvsv"
+          
+      },
     {
       key: 3,
       _id: 3,
@@ -27,7 +36,11 @@ export default {
       Artist: "nsdonviv",
       Desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       Title: "nft3",
-      Price: "23 $celo"
+      Price: "23 $celo",
+      Editions: "3",
+      Type: "img",
+      Royalties: "10%",
+      Link: "service-storage.com/sdsvsv"
     },
     {
         key: 4,
@@ -36,7 +49,11 @@ export default {
         Artist: "nsdonviv",
         Desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         Title: "nft4",
-        Price: "20 $celo"
+        Price: "20 $celo",
+        Editions: "9",
+        Type: "img",
+        Royalties: "1%",
+        Link: "service-storage.com/ssccsdf"
     },
     {
       key: 5,
@@ -45,7 +62,11 @@ export default {
       Artist: "nsdonviv",
       Desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       Title: "nft5",
-      Price: "98 $celo"
+      Price: "98 $celo",
+      Editions: "1",
+      Type: "img",
+      Royalties: "4%",
+      Link: "service-storage.com/dcnsidnis"
     },
     {
         key: 6,
@@ -54,7 +75,11 @@ export default {
         Artist: "nsdonviv",
         Desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         Title: "nft6",
-        Price: "92 $celo"
+        Price: "92 $celo",
+        Editions: "1",
+        Type: "gif",
+        Royalties: "10%",
+        Link: "service-storage.com/cmosdn"
     },
     {
         key: 7,
@@ -63,7 +88,11 @@ export default {
         Artist: "nsdonviv",
         Desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         Title: "nft7",
-        Price: "10 $celo"
+        Price: "10 $celo",
+        Editions: "1",
+        Type: "mp4/video",
+        Royalties: "20%",
+        Link: "service-storage.com/anmosncoa"
     },
     {
         key: 8,
@@ -72,7 +101,11 @@ export default {
         Artist: "nsdonviv",
         Desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         Title: "nft8",
-        Price: "81 $celo"
+        Price: "81 $celo",
+        Editions: "5",
+        Type: "img",
+        Royalties: "15%",
+        Link: "service-storage.com/aooawnoanwonw"
     },
     {
         key: 9,
@@ -81,7 +114,11 @@ export default {
         Artist: "nsdonviv",
         Desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         Title: "nft9",
-        Price: "12 $celo"
+        Price: "12 $celo",
+        Editions: "10",
+        Type: "gif",
+        Royalties: "4%",
+        Link: "service-storage.com/cnsdnoaoa"
     }
   ]
 }
