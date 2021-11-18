47e89bcce979a950bd958d0054569bbb7f58fabc
SiddanshChawla <chawlasiddansh@gmail.com>
2021-11-18 22:24:24 +0530

    update

--- eclair-frontend/src/constants 2.js
+++
@@ -1,3 +0,0 @@
-export const NFTAddress = "0x085342CF031eb31E813f72Af306d3670289E0a48";
-export const NFTMarketAddress = "0x47B8DAc1e25314AC1cAB9A7ddF606B7cdA6448bF";
-export const ScoreAddress = "0x0D1B1daeaD23a49E336d2F081C972be62b094A1B";
